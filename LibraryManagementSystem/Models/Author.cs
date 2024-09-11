a2c6c8d0adc09d0094dc3f4d077874852df61473
Gizem Çalışkan <gizemcaliskan24@gmail.com>
2024-09-11 17:01:23 +0300

    Add models

---
+++ LibraryManagementSystem/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+       
+        [Required(ErrorMessage = "İsim zorunludur")]
+        [StringLength(100, ErrorMessage = "İsim 100 karakterden uzun olmamalıdır")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Doğum Tarihi zorunludur")]
+        public DateTime BirthDate { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Başlık zorunludur")]
+        [StringLength(200, ErrorMessage = "Başlık 200 karakterden uzun olmamalıdır")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Yayın Yılı zorunludur")]
+        [Range(1, int.MaxValue, ErrorMessage = "Yayın Yılı 0'dan büyük olmalıdır")]
+        public int PublishedYear { get; set; }
+
+        [Required(ErrorMessage = "Fiyat zorunludur")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Fiyat 0'dan büyük olmalıdır")]
+        public decimal Price { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
